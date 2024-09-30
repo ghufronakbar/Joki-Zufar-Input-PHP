56109e81bc03470ef096b8afe0b9d5a8c984d162
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-30 21:59:37 +0700

    add aritmathics

--- index.php
+++ index.php
@@ -9,43 +9,47 @@
 
 <body>
 
-<?php
-if (isset($_POST['submit'])) {
-    $nama = $_POST['nama'];
-    $bilpertama = $_POST['bilpertama'];
-    $bilkedua = $_POST['bilkedua'];
-
-    echo "<p>Nama: " . htmlspecialchars($nama) . "</p>";
-    echo "<p>Bilangan Pertama: " . htmlspecialchars($bilpertama) . "</p>";
-    echo "<p>Bilangan Kedua: " . htmlspecialchars($bilkedua) . "</p>";
-    echo "<br>";
-    
-    echo '<form action="index.php" method="post">';
-    echo '<button type="submit">Isi Lagi</button>';
-    echo '</form>';
-} else {    
-    ?>
-    <form action="index.php" method="post">
-        <table>
-            <tr>
-                <td>Masukkan Nama anda</td>
-                <td>: <input type="text" name="nama" required></td>
-            </tr>
-            <tr>
-                <td>Masukkan Bilangan Pertama</td>
-                <td>: <input type="number" name="bilpertama" required></td>
-            </tr>
-            <tr>
-                <td>Masukkan Bilangan Kedua</td>
-                <td>: <input type="number" name="bilkedua" required></td>
-            </tr>
-        </table>
-        <button type="submit" name="submit">Submit</button>
-    </form>
     <?php
-}
-?>
+    if (isset($_POST['submit'])) {
+        $nama = $_POST['nama'];
+        $bilpertama = $_POST['bilpertama'];
+        $bilkedua = $_POST['bilkedua'];
 
+        echo "<p>Nama: <span style='font-weight:bold'>" . htmlspecialchars($nama) . "</span></p>";
+        echo "<p>Bilangan Pertama: <span style='font-weight:bold'>" . htmlspecialchars($bilpertama) . "</span></p>";
+        echo "<p>Bilangan Kedua: <span style='font-weight:bold'>" . htmlspecialchars($bilkedua) . "</span></p>";
+        echo "<br>";
+        echo "<h2>Perhitungan Aritmatika</h2>";
+        echo "<p>" . $bilpertama . " + " . $bilkedua . " = " . ($bilpertama + $bilkedua) . "</p>";
+        echo "<p>" . $bilpertama . " - " . $bilkedua . " = " . ($bilpertama - $bilkedua) . "</p>";
+        echo "<p>" . $bilpertama . " * " . $bilkedua . " = " . ($bilpertama * $bilkedua) . "</p>";
+        echo "<p>" . $bilpertama . " / " . $bilkedua . " = " . ($bilpertama / $bilkedua) . "</p>";
+        echo "<p>" . $bilpertama . " % " . $bilkedua . " = " . ($bilpertama % $bilkedua) . "</p>";
+        echo '<form action="index.php" method="post">';
+        echo '<button type="submit">Isi Lagi</button>';
+        echo '</form>';
+    } else {
+    ?>
+        <form action="index.php" method="post">
+            <table>
+                <tr>
+                    <td>Masukkan Nama anda</td>
+                    <td>: <input type="text" name="nama" required></td>
+                </tr>
+                <tr>
+                    <td>Masukkan Bilangan Pertama</td>
+                    <td>: <input type="number" name="bilpertama" required></td>
+                </tr>
+                <tr>
+                    <td>Masukkan Bilangan Kedua</td>
+                    <td>: <input type="number" name="bilkedua" required></td>
+                </tr>
+            </table>
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    <?php
+    }
+    ?>
 </body>
 
 </html>
